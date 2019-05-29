78131d3e5c977fc3ae1233997298bae6fa0bb460
jasonandjay <342690199@qq.com>
2019-05-29 12:00:27 +0800

    实现props子向父

---
+++ swiper/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <p>当前最热门的五大网游</p>
+    <p>
+      <span v-for="(item, index) in list" :key="item" :class="cur==index?'active':''" @click="change(index)">{{item}}</span>
+    </p>
+    <Swiper :cur="cur" :change="change"/>
+  </div>
+</template>
+
+<script>
+import Swiper from './components/Swiper'
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      cur: 0,
+      list: ['英雄联盟','守望先锋','穿越火线','绝地求生','贪玩蓝月']
+    }
+  },
+  components: {
+    Swiper
+  },
+  methods: {
+    change(cur){
+      this.cur = cur;
+    }
+  }
+}
+</script>
+
+<style>
+*{
+  padding: 0;
+  margin: 0;
+  list-style: none;
+}
+ul,li{
+  width: 100%;
+}
+img{
+  width: 100%;
+}
+.active{
+  color: lightcoral;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ swiper/src/components/Swiper.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <p>{{cur}}</p>
+    <ul  class="swiper-container">
+       <div class="swiper-wrapper">
+         <li v-for="(item, index) in posts" :key="item" class="swiper-slide">
+          <img :src="item">
+        </li>
+      </div>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: ['cur', 'change'],
+  data () {
+    return {
+      posts: [
+        'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1513222572,1724296872&fm=26&gp=0.jpg',
+        'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=119991635,3338844579&fm=26&gp=0.jpg',
+        'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3751132997,2377190187&fm=26&gp=0.jpg',
+        'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3667338627,4040384263&fm=26&gp=0.jpg',
+        'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1588770783,3837654158&fm=26&gp=0.jpg'
+      ]
+    }
+  },
+  mounted(){
+    const that = this;
+    this.swiper = new Swiper('ul', {
+      on:{
+        slideChange: function(cur){
+          console.log('当前下标...', this.activeIndex);
+          that.change(this.activeIndex);
+        }
+      }
+    })
+  },
+  updated(){
+    console.log('this.cur...', this.cur);
+    this.swiper.slideTo(this.cur);
+  }
+}
+</script>
